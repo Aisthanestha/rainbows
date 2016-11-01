8326dd7aa06d90dbb23adc9821dabfba95bf5801
aisthanestha <ghendrickson47@gmail.com>
2016-11-01 18:33:31 -0500

    adding stuff

--- rtgener.py
+++ rtgener.py
@@ -1,8 +1,12 @@
 import os
-# this will create a rainbow table that is loweralpha-numerica abcd...1234... with a plaintext length min of 6 and max of 6 with table index from 0 to 10 so it will take a while to generate. 
+
 print "start of script..."
+
 os.system("cd /usr/share/rainbowcrack")
 for i in range (0, 10):
-        os.system("rtgen md5 loweralpha-numeric 6 6 %s 3500 100000 0" % i)# hash is MD5, looks for lower letters, with numbers with a min and max of 6 chars total. for more charsets look at /usr/share/rainbowcrack/charset.txt
-os.system("rtsort *.rt") #sorts all .rt files in /usr/share/rainbowcrack
+        os.system("rtgen md5 loweralpha-numeric 6 6 %s 3500 100000 0" % i)
+os.system("rtsort *.rt") 
+hashFile= raw_input("Give path to hash.txt")
+os.system("mv %s /usr/share/rainbowcrack/" % hashFile)
+
 print "end of script..."
