e19ae5f125b983dd46a3197cb2cddd3f93180846
aisthanestha <ghendrickson47@gmail.com>
2016-11-01 18:47:57 -0500

    mayb

--- rtgener.py
+++ rtgener.py
@@ -6,7 +6,7 @@ os.system("cd /usr/share/rainbowcrack")
 for i in range (0, 10):
         os.system("rtgen md5 loweralpha-numeric 6 6 %s 3500 100000 0" % i)
 os.system("rtsort *.rt") 
-hashFile= raw_input("Give path to hash.txt")
-os.system("mv %s /usr/share/rainbowcrack/" % hashFile)
+mkHash= raw_input("string to be made into md5 hash")
+os.system("echo -n %s | md5sum " % mkHash)
 
 print "end of script..."
