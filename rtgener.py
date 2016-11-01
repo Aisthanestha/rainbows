37c45b0b4c0fa9a8e2cf9e09dde7b43069862ca9
aisthanestha <ghendrickson47@gmail.com>
2016-11-01 18:51:15 -0500

    ooooh

--- rtgener.py
+++ rtgener.py
@@ -7,6 +7,9 @@ for i in range (0, 10):
         os.system("rtgen md5 loweralpha-numeric 6 6 %s 3500 100000 0" % i)
 os.system("rtsort *.rt") 
 mkHash= raw_input("string to be made into md5 hash")
-os.system("echo -n %s | md5sum " % mkHash)
+os.system("echo -n %s | md5sum >> hash.txt" % mkHash)
+os.system("rcrack *.rt -l hash.txt")
+
+
 
 print "end of script..."
